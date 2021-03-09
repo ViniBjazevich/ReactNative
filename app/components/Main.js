c972d39958a786cfc86851fc137dbf8633bcb3f1
Vini Bjazevich <vinibjazevich97@gmail.com>
2021-03-08 22:23:20 -0800

    Cookie image can increase count. Buttons now are round and created a modal

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image, Modal, Alert, Pressable } from 'react-native';
 
 
 export default function Main() {
   const [total, setTotal] = useState(0);
   const [intervalAmount, setIntervalAmount] = useState(1000000000000);
   const [tapPower, setTapPower] = useState(1);
-  const [currentIndex, setCurrentIndex] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [modalVisible, setModalVisible] = useState(false);
 
   const intervalArray = [1000, 900, 800, 700, 600, 500, 400, 300, 200, 100, 50];
 
@@ -27,22 +28,52 @@ export default function Main() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.bigBlue}>{total}</Text>
-      <Button
-        onPress={() => {
-          setTotal((prevTotal) => prevTotal + tapPower)
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+          setModalVisible(!modalVisible);
         }}
-        title="More Cookies!"
-      />
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Text style={styles.bigBlue}>{total}</Text>
+      <TouchableOpacity onPress={() => {
+          setTotal((prevTotal) => prevTotal + tapPower)
+        }}>
+        <Image
+          style={styles.tinyLogo}
+          source={{
+            uri: 'https://test-projects-1.s3-us-west-1.amazonaws.com/cookie-removebg-preview.png',
+          }}
+        />
+
+      </TouchableOpacity>
       <View style={styles.buttonRow}>
-        <TouchableOpacity onPress={ () => updateInterval()}>
+        <TouchableOpacity onPress={ () => {
+          // updateInterval()
+          setModalVisible(true)
+          console.log('I was clicked')
+        }}>
           <View style={styles.buttons}>
-            <Text>Auto tap</Text>
+            <Text style={{color: 'white'}}>Auto tap</Text>
           </View>
         </TouchableOpacity>
         <TouchableOpacity onPress={ () => setTapPower((prev) => prev * 2)}>
           <View style={styles.buttons}>
-            <Text>Tap power</Text>
+            <Text style={{color: 'white'}}>Tap power</Text>
           </View>
         </TouchableOpacity>
       </View>
@@ -73,7 +104,36 @@ const styles = StyleSheet.create({
   },
   buttons: {
     width: 100,
-    backgroundColor: 'blue',
-    height: 50
-  }
+    backgroundColor: 'black',
+    height: 50,
+    borderRadius: 20,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  tinyLogo: {
+    width: 100,
+    height: 100,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
 });
